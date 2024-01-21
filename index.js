cc8d913ef67eaef37989c70100a5718fed1b72a7
hanaihui <155342636+hanaihui@users.noreply.github.com>
2024-01-21 18:34:43 +0800

    增加resume的空壳 更新了index的渐入动画

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-let carousel = document.querySelector("#carousel");
-let items = carousel.children;
+let items = document.querySelector("#carousel").children;
 let index = 0;
 setInterval(() => {
   items[index].className = "";
   index++;
   if (index === 4) { index = 0; }
   items[index].className = "active";
-}, 10000);
+}, 5000);
